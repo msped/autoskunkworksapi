8752d6a505a11000f26836d762a13353a840db5c
Matt Edwards <mattsp.edwards@googlemail.com>
2022-08-14 17:59:20 +0100

    feat: added customer user

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    profile_picture = models.ImageField(upload_to="profile_pic", null=True, blank=True)
+    instagram = models.URLField(null=True, blank=True)
